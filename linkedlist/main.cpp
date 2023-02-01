b8c631e84f7081c5aa5ba5a38baf1caa35b45dd3
void <void@gmail.com>
2023-01-31 19:28:41 -0800

    Initial Commit

---
+++ linkedlist/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  linkedlist
+//
+//  Created by Mark Garcia on 1/31/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
